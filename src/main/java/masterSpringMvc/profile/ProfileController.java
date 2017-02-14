277dc72b14cfd43d9107260f8a2ed58839b705c5
Michal Pietrzak <201011@student.pwr.wroc.pl>
2017-02-14 21:34:39 +0100

    Add form page with validation mechanism.

---
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -0,0 +1,35 @@
+package masterSpringMvc.profile;
+
+import java.util.Locale;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import masterSpringMvc.USLocalDateFormatter.USLocalDateFormatter;
+
+@Controller
+public class ProfileController {
+	@RequestMapping("/profile")
+	public String displayProfile(ProfileForm profileForm) {
+		return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "profile/profilePage";
+		}
+		System.out.println("Pomyślnie zapisany profil " + profileForm);
+		return "redirect:/profile";
+	}
+
+	@ModelAttribute("dateFormat")
+	public String localeFormat(Locale locale) {
+		return USLocalDateFormatter.getPattern(locale);
+	}
+}

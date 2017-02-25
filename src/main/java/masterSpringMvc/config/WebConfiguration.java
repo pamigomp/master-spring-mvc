a73a34544b92750dafdb034d1541582e53f4198c
Michal Pietrzak <201011@student.pwr.wroc.pl>
2017-02-25 23:57:41 +0100

    Add Swagger2.

--- src/main/java/masterSpringMvc/config/WebConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebConfiguration.java
@@ -23,8 +23,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import masterSpringMvc.date.USLocalDateFormatter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
@@ -60,6 +64,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 			}
 		};
 		return embeddedServletContainerCustomizer;
+	}
+
+	@Bean
+	public Docket userApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(path -> path.startsWith("/api/")).build();
 	}
 
 	@Bean

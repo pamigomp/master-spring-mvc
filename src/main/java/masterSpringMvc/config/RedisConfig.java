4089c76cd72f030f8bf4aa6fb7b85d8fb94ffa42
Michal Pietrzak <201011@student.pwr.wroc.pl>
2017-03-06 21:47:25 +0100

    Add heroku support.

--- src/main/java/masterSpringMvc/config/RedisConfig.java
+++ src/main/java/masterSpringMvc/config/RedisConfig.java
@@ -1,12 +1,35 @@
 package masterSpringMvc.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @Profile("redis")
 @EnableRedisHttpSession
 public class RedisConfig {
-
+	@Bean
+	@Profile("heroku")
+	public RedisConnectionFactory redisConnectionFactory() throws URISyntaxException {
+		JedisConnectionFactory redis = new JedisConnectionFactory();
+		String redisUrl = System.getenv("REDI_URL");
+		URI redisUri = new URI(redisUrl);
+		redis.setHostName(redisUri.getHost());
+		redis.setPort(redisUri.getPort());
+		redis.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+		return redis;
+	}
+	
+	@Bean
+	@Profile({ "cloud", "heroku" })
+	public static ConfigureRedisAction configureRedisAction() {
+		 return ConfigureRedisAction.NO_OP;
+	}
 }
